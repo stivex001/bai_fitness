8e6cd5db7c799fad70da36926d8e62e291b8d27e
stivex001 <stephenadeyemo@gmail.com>
2023-04-12 18:02:24 +0100

    feat: getting started with typescript component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Todos = () => {
+const Todos: React.FC<{items: string[]}> = ({items}) => {
   return (
-    <div className='text-3xl font-bold underline'>Todos</div>
+    <ul className='m-5'>
+        {items.map((item) => (
+            <li className='font-bold' key={item}>{item}</li>
+        ))}
+    </ul>
   )
 }
 
