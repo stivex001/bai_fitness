f7220c81d22eb7a87f110cc1be219e47bfce3ade
stivex001 <stephenadeyemo@gmail.com>
2023-04-18 12:19:03 +0100

    feat: added infered type to useState value

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "./scenes/navbar/Navbar";
 
 function App() {
-  const [selectedPage, setSelectPage] = useState('Home')
+  const [selectedPage, setSelectPage] = useState<string>('home')
 
   return <div className=" bg-[#1e1e1e]">
     <Navbar selectedPage={selectedPage} setSelectPage={setSelectPage} />
