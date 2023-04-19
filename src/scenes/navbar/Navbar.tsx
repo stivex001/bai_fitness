34c4376d5d78e2457a1ebd56a75bfbb06ca40cc9
stivex001 <stephenadeyemo@gmail.com>
2023-04-20 00:11:55 +0100

    feat: implemented  mobileresponsivenes to navbar

--- src/scenes/navbar/Navbar.tsx
+++ src/scenes/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import MenuIcon from "@mui/icons-material/Menu";
 import ActionButton from "@/shared/ActionButton";
+import CloseIcon from "@mui/icons-material/Close";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -28,7 +29,7 @@ const Navbar = ({ selectedPage, setSelectPage }: Props) => {
           {/* Right */}
           {isAboveMediumScreens ? (
             <div className={`${flexBetween} gap-16`}>
-              <div className={`${flexBetween} gap-8`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
                 <Links
                   page="Home"
                   selectedPage={selectedPage}
@@ -67,6 +68,39 @@ const Navbar = ({ selectedPage, setSelectPage }: Props) => {
           )}
         </div>
       </div>
+
+      {/* MOBILE MENU MOBILE */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-60 bg-red-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <CloseIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          <div className={` w-full ml-[33%] flex flex-col gap-10 text-2xl`}>
+            <Links
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectPage={setSelectPage}
+            />
+            <Links
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectPage={setSelectPage}
+            />
+            <Links
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectPage={setSelectPage}
+            />
+            <Links
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectPage={setSelectPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
