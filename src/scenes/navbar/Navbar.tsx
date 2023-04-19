f676bdeaad010e7308ccc62ad990ce983b940079
stivex001 <stephenadeyemo@gmail.com>
2023-04-19 19:38:09 +0100

    fix: uninsatlled some redundants packages

--- src/scenes/navbar/Navbar.tsx
+++ src/scenes/navbar/Navbar.tsx
@@ -1,52 +1,64 @@
-import React from 'react'
-import Links from './Links'
-import { SelectedPage } from '@/shared/types';
+import React from "react";
+import Links from "./Links";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
   setSelectPage: (value: SelectedPage) => void;
-}
+};
 
-const Navbar = ({selectedPage, setSelectPage}: Props) => {
-  const flexBetween = "flex items-center justify-between"
+const Navbar = ({ selectedPage, setSelectPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
   return (
     <nav>
-      <div className={`${flexBetween} py-6 fixed top-0 z-30 w-full bg-[#1e1e1e] `}>
+      <div
+        className={`${flexBetween} py-6 fixed top-0 z-30 w-full bg-[#1e1e1e] `}
+      >
         <div className={`${flexBetween} w-5/6 mx-auto text-white `}>
-          
-        {/* Logo */}
-        <div className='w-full'>
-          <p>WORKOUT</p>
-        </div>
-        {/* Right */}
-        <div className={`${flexBetween} gap-16 w-full`}>
-          <div className={`${flexBetween} gap-8`}>
-            <Links 
-              page="Home"
-              selectedPage={selectedPage}
-              setSelectPage={setSelectPage} />
-            <Links 
-              page="Benefits"
-              selectedPage={selectedPage}
-              setSelectPage={setSelectPage} />
-            <Links 
-              page="Our Classes"
-              selectedPage={selectedPage}
-              setSelectPage={setSelectPage} />
-            <Links 
-              page="Contact Us"
-              selectedPage={selectedPage}
-              setSelectPage={setSelectPage} />
+          {/* Logo */}
+          <div className="w-full">
+            <p>WORKOUT</p>
           </div>
-          <div className={`${flexBetween} gap-8`}>
-            <p>Sign In</p>
-            <button>Become a Member</button>
-          </div>
-        </div>
+          {/* Right */}
+          {isAboveMediumScreens ? (
+            <div className={`${flexBetween} gap-16 w-full`}>
+              <div className={`${flexBetween} gap-8`}>
+                <Links
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectPage={setSelectPage}
+                />
+                <Links
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectPage={setSelectPage}
+                />
+                <Links
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectPage={setSelectPage}
+                />
+                <Links
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectPage={setSelectPage}
+                />
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          ) : (
+            <div>ygydgygydgyu </div>
+          )}
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
