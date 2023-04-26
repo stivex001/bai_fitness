d7d433abd367819249c81884b7a61e4442d07209
stivex001 <stephenadeyemo@gmail.com>
2023-04-26 10:51:10 +0100

    fix: code reafctored

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -9,8 +9,8 @@ const Class = ({ name, desc, image }: Props) => {
   return (
     <li className="relative mx-5 inline-block h-[380px] w-[450px]">
       <div className={overLayStyles}>
-        <p>{name}</p>
-        <p>{desc}</p>
+        <p className="text-2xl">{name}</p>
+        <p className="mt-5">{desc}</p>
       </div>
       <img src={image} alt={image} />
     </li>
